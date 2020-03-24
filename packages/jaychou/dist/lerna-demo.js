90d52345c150ebdf8f8dbe8d5f01455e48256718
Vicen Law <vicenlaw@gmail.com>
2020-03-24 09:06:50 +0800

    0.1.0 build

--- packages/jaychou/dist/lerna-demo.js
+++ packages/jaychou/dist/lerna-demo.js
@@ -1,12 +1,19 @@
 /*!
- * jaychou v0.0.4
+ * jaychou v0.1.0
  * (c) 2020-2020 Vicen Law <vicenlaw@gmail.com>
  * Github https://github.com/VicenLaw/lerna-demo
  * Released under the MIT License.
  */
 function lernaDemo() {
-  console.log('jay chou');
-  return 'jay chou';
+  console.log("jay chou");
+  return "jay chou";
+}
+function getJay() {
+  var songs = ["可爱女人", "完美主义", "星晴", "娘子", "斗牛", "黑色幽默", "伊斯坦堡", "印第安老斑鸠", "龙卷风", "反方向的钟"];
+  return songs;
+}
+function getAlbums() {
+  return ["Jay"];
 }
 
-export { lernaDemo };
+export { getAlbums, getJay, lernaDemo };

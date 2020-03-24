edf4a62cd607ee946135ef260de04a07d0230f43
Vicen Law <vicenlaw@gmail.com>
2020-03-24 09:06:16 +0800

    0.1.0

--- packages/jaychou/src/index.js
+++ packages/jaychou/src/index.js
@@ -1,4 +1,24 @@
 export function lernaDemo() {
-  console.log('jay chou');
-  return 'jay chou';
+  console.log("jay chou");
+  return "jay chou";
+}
+
+export function getJay() {
+  const songs = [
+    "可爱女人",
+    "完美主义",
+    "星晴",
+    "娘子",
+    "斗牛",
+    "黑色幽默",
+    "伊斯坦堡",
+    "印第安老斑鸠",
+    "龙卷风",
+    "反方向的钟"
+  ];
+  return songs;
+}
+
+export function getAlbums() {
+  return ["Jay"];
 }
